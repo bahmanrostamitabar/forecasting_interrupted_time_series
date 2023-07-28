148915cbe216604e9b2b21ae1b949c6edd2d7a0e
Rob J Hyndman <robjhyndman@gmail.com>
2023-07-28 21:18:26 +0100

    Cleaned up some of tourism example

--- _targets.R
+++ _targets.R
@@ -14,77 +14,86 @@ tar_source()
 
 # List of targets
 list(
-  # Tourism example
-  tar_target(tourism_csv, here::here("data/340101.xlsx"), format="file"),
+  # Tourism data ---------------------------------------------------------------
+  tar_target(tourism_csv, here::here("data/340101.xlsx"), format = "file"),
   tar_target(austa, read_tourism(tourism_csv)),
-  # Fit ETS model
-  tar_target(fit_tourism,  
-    austa |>
-      filter(Month < yearmonth("2020 Jan")) |>
-      model(ETS(Visitors))),
-  tar_target(tourism_plot1, tourism_plot(austa) + tourism_labels),
+  # Tourism time plot
   tar_target(tourism_labels, labs(
     x = "Month",
     y = "Thousands of visitors",
     title = "Total short-term visitors to Australia"
   )),
-  tar_target(tourism_history, 
-             geom_line(data = austa |> filter(Month >= yearmonth("2016 Jan")), 
-                       aes(y=Visitors))
+  tar_target(tourism_plot1, tourism_plot(austa) + tourism_labels),
+  # Forecasts
+  tar_target(tsol1, tourism_sol1(austa)),
+  tar_target(tsol2, tourism_sol2(austa)),
+  tar_target(tsol3, tourism_sol3(austa)),
+  tar_target(tsol4, tourism_sol4(austa)),
+  # Forecast plots
+  tar_target(
+    tourism_history,
+    geom_line(
+      data = austa |> filter(Month >= yearmonth("2016 Jan")),
+      aes(y = Visitors)
+    )
   ),
-  # ETS forecasts
-  tar_target(fc_tourism, forecast(fit_tourism, h = 48)),
-  tar_target(tsol1, tourism_sol1(austa)),                            
-  tar_target(tsol2, tourism_sol2(austa)),                            
-  tar_target(tsol3, tourism_sol3(austa)),                            
-  tar_target(tsol4, tourism_sol4(austa)),   
-  tar_target(tsol1_plot, 
-    autoplot(tsol1, level=90) + tourism_history + tourism_labels
+  tar_target(
+    tsol1_plot,
+    autoplot(tsol1, level = 90) + tourism_history + tourism_labels
   ),
-  tar_target(tsol2_plot, 
-    autoplot(tsol2, level=90) +  tourism_history + tourism_labels
+  tar_target(
+    tsol2_plot,
+    autoplot(tsol2, level = 90) + tourism_history + tourism_labels
   ),
-  tar_target(tsol3_plot, 
-    autoplot(tsol3, level=90)  + tourism_history + tourism_labels
+  tar_target(
+    tsol3_plot,
+    autoplot(tsol3, level = 90) + tourism_history + tourism_labels
   ),
-  tar_target(tsol4_plot, 
-    autoplot(tsol4, level=90) + tourism_history + tourism_labels
+  tar_target(
+    tsol4_plot,
+    autoplot(tsol4, level = 90) + tourism_history + tourism_labels
   ),
 
   # Attended incidents example
-  tar_target(attendant_csv, here::here("data/attendent_incident.csv"), format="file"),
-  tar_target(verified_csv, here::here("data/verified_incident.csv"), format="file"),
+  tar_target(attendant_csv, here::here("data/attendent_incident.csv"), format = "file"),
+  tar_target(verified_csv, here::here("data/verified_incident.csv"), format = "file"),
   tar_target(attendant, readr::read_csv(attendant_csv)),
   tar_target(verified, readr::read_csv(verified_csv)),
   tar_target(df, create_tsibble(attendant, verified)),
   tar_target(attended_plot, autoplot(df, Attended)),
-  tar_target(stl, # STL Decomposition
+  tar_target(
+    stl, # STL Decomposition
     df |>
       filter(Area == "BC") |>
       model(
         stl = STL(Attended ~ trend(window = 81) + season(period = 7, window = Inf)),
       )
   ),
-  tar_target(stl_plot,
+  tar_target(
+    stl_plot,
     stl |>
-    components() |>
-    autoplot()
+      components() |>
+      autoplot()
   ),
-  tar_target(stl_model,
+  tar_target(
+    stl_model,
     decomposition_model(
       STL(Attended ~ trend(window = 81) + season(period = 7, window = Inf)),
       ETS(season_adjust ~ season("N"))
     )
   ),
-  tar_target(fit,
+  tar_target(
+    fit,
     df |>
       model(
         ets = ETS(Attended),
         arima = ARIMA(Attended),
         stl_ets = stl_model
       )
-    ),
-  tar_target(fc, forecast(fit, h=365)),
+  ),
+  tar_target(fc, forecast(fit, h = 365)),
   tar_quarto(paper,
-    "fits.qmd", extra_files = "references.bib")
+    "fits.qmd",
+    extra_files = "references.bib"
+  )
 )
